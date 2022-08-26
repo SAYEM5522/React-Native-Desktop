dd3449b3578291ac789f75ad3033217de9f2e8ec
Habib0905 <95955904+Habib0905@users.noreply.github.com>
2022-08-26 19:39:05 +0600

    Update Description.js

--- Description.js
+++ Description.js
@@ -3,8 +3,8 @@ export const Description=[
     id:1,
     name:"Spotify",
     description:"Spotify is a streaming music service that allows users to listen to music while on the go. Users can create playlists, search for music, and listen to the music they love.It is one of the largest music streaming service providers, with over 433 million monthly active users, including 188 million paying subscribers, as of June 2022.The basic, ad-supported version of Spotify is completely free. It offers unlimited access to the full catalog of music and podcasts but comes with a few drawbacks. The most intrusive are frequent ad breaks. These can really kill your groove if you're listening at a party.",
-    Rating:1000,
-    type:"Movie",
+    Rating:2000,
+    type:"Music",
     list:"Free",
     ScreenShoot:[
       "https://www.androidheadlines.com/wp-content/uploads/2020/08/spotify-new-tablet-interface-1.jpg",
@@ -15,26 +15,26 @@ export const Description=[
     company:"Spotify Inc.",
     Features:"",
     AvgRating:4.5,
-    Img:"https://store-images.s-microsoft.com/image/apps.30432.13571498826857201.00a9d390-581f-492c-b148-b2ce81649480.b4ea65c8-8446-470b-a904-1080ffacc04e?w=100",
-    BannerImg:"https://canvas-cdn-prod.azureedge.net/assets/a6/5b/a65b08ba-0630-4566-bf0f-8f5d4810a71e.jpg?n=AppsForGamers-WW-Tile-3840x2160-W11-01.jpg"
+    Img:"https://cdn.wallpapersafari.com/7/6/8qH14d.jpg",
+    BannerImg:"https://cdn.dribbble.com/users/2936992/screenshots/11128383/banner_4x.png"
   },
   {
     id:2,
     name:"Netflix",
-    description:"Spotify is a streaming music service that allows users to listen to music while on the go. Users can create playlists, search for music, and listen to the music they love.",
+    description:"Netflix has something for everyone. Watch TV shows and movies recommended just for you, including award-winning Netflix original series, movies, and documentaries. There’s even a safe watching experience just for kids with family-friendly entertainment.Now on Windows, you can enjoy every detail of the world’s favorite shows in 4K Ultra HD on Netflix. Download many of your favorite series and movies with the simple click of the download button. You can watch while you’re on the go or without an Internet connection on your PC, tablet or laptop with Windows 10.Instantly watch TV shows and movies through thousands of internet-connected devices. You can play, pause, and resume watching, all without commercials. Netflix adds new content all the time. Browse titles or search for your favorites. The more you watch, the better Netflix gets at recommending TV shows and movies that you’ll love — just for you. You can create up to five individual profiles within a single Netflix account. Profiles allow different members of your household to have their own personalized Netflix experience built around the movies and TV shows they enjoy. If you decide Netflix isn't for you - no problem. No contract, no cancellation fees, no commitment. Cancel online anytime.",
     Rating:1000,
     type:"Movie",
     list:"Free",
     ScreenShoot:[
-      "https://wallpapercave.com/wp/wp7630844.jpg",
-      "https://img.gadgethacks.com/img/99/89/63629569232733/0/spotify-codes-now-makes-easier-share-dope-music-heres.w1456.jpg",
-      "https://img.gadgethacks.com/img/99/89/63629569232733/0/spotify-codes-now-makes-easier-share-dope-music-heres.w1456.jpg",
-      "https://img.gadgethacks.com/img/99/89/63629569232733/0/spotify-codes-now-makes-easier-share-dope-music-heres.w1456.jpg",
+      "https://cdn.mos.cms.futurecdn.net/YDQnzkVbJbaNGpQfuN6co4-1200-80.jpg",
+      "https://d3dh6of9cnaq4t.cloudfront.net/Pictures/1024x536/3/5/3/11353_Netflix-menu-screen.png",
+      "https://www.androidauthority.com/wp-content/uploads/2019/06/marvel-netflix-search-results.jpg",
+      "https://cdn.mos.cms.futurecdn.net/uUiKjPTNAQERpcAV2KDiha.jpg",
     ],
-    company:"Spotify",
+    company:"Netflix",
     Features:"",
     AvgRating:4.5,
-    Img:"https://wallpapercave.com/wp/wp7630844.jpg"
+    Img:"https://wallpaperaccess.com/full/2772922.png"
 
   },
   {
